e690d8f9d974d89a8de88e789e76902eb6b81abf
mihaiicey <mihai@icey.ro>
2023-06-02 16:27:35 +0300

    finalizare mobile menu

--- components/share/menues/MobileMen.tsx
+++ components/share/menues/MobileMen.tsx
@@ -16,10 +16,11 @@ const MobileMenu: NextPage = () => {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex w-full justify-center rounded-md  px-2 py-2 text-sm font-medium text-primary border-primary border-2 focus:outline-none"
-            onClick={handleToggleActive}
+        <Menu.Button
+          className="inline-flex w-full justify-center rounded-md  px-2 py-2 text-sm font-medium text-primary border-primary border-2 focus:outline-none"
+          onClick={handleToggleActive}
         >
-        {buttonactv ? (
+          {buttonactv ? (
             <HiXMark className="text-2xl" />
           ) : (
             <HiBars3BottomLeft className="text-2xl" />
@@ -37,17 +38,20 @@ const MobileMenu: NextPage = () => {
       >
         <Menu.Items className="absolute left-0 mt-4 w-[96vw] z-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1 ">
-            <Menu.Item>
+            {HeaderMenuRo.map((item, index) => (
+              <Menu.Item key={index}>
               {({ active }) => (
-                <button
+                <Link
+                  href={item.url}
                   className={`${
-                    active ? "bg-violet-500 text-white" : "text-gray-900"
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    router.pathname === item.url ? "text-orange" : "text-primary"
+                  } group flex w-full items-center rounded-md px-2 py-2 text-sm font-semibold`}
                 >
-                  Edit
-                </button>
+                  {item.title}
+                </Link>
               )}
             </Menu.Item>
+            ))}
           </div>
         </Menu.Items>
       </Transition>
